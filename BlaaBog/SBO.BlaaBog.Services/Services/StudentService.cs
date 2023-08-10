28937b3833c7c677e91e88cb512100e7a3b9f89d
Cryze <sammyschroeder123@gmail.com>
2023-08-10 11:03:21 +0200

    Student and Teacher Service

--- BlaaBog/SBO.BlaaBog.Services/Services/StudentService.cs
+++ BlaaBog/SBO.BlaaBog.Services/Services/StudentService.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using SBO.BlaaBog.Domain.Connections;
+using SBO.BlaaBog.Domain.Entities;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,5 +10,118 @@ namespace SBO.BlaaBog.Services.Services
 {
     public class StudentService
     {
+
+        private StudentConnection _studentConnection;
+        public StudentService(TeacherConnection teacherConnection)
+        {
+            _studentConnection = new StudentConnection();
+        }
+
+        #region Create
+
+        /// <summary>
+        /// Creates a student in the database
+        /// </summary>
+        /// <param name="student"></param>
+        /// <returns> bool </returns>
+        public async Task<bool> CreateStudentAsync(Student student)
+        {
+            return await _studentConnection.CreateStudentAsync(student);
+        }
+
+        #endregion
+
+        #region Read
+
+        /// <summary>
+        /// Gets a student from the database
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns> Student </returns>
+        public async Task<Student?> GetStudentAsync(int id)
+        {
+            return await _studentConnection.GetStudentAsync(id);
+        }
+
+        /// <summary>
+        /// Gets all students from the database
+        /// </summary>
+        /// <returns> List<Student> </returns>
+        public async Task<List<Student>?> GetStudentsAsync()
+        {
+            return await _studentConnection.GetStudentsAsync();
+        }
+
+        /// <summary>
+        /// Gets all students from the database that contain the name given
+        /// </summary>
+        /// <param name="name"></param>
+        /// <returns> List<Student> </returns>
+        public async Task<List<Student>?> GetStudentsByNameAsync(string name)
+        {
+            return await _studentConnection.GetStudentsByNameAsync(name);
+        }
+
+        /// <summary>
+        /// Gets a student from the database by email
+        /// </summary>
+        /// <param name="email"></param>
+        /// <returns> Student </returns>
+        public async Task<Student?> GetStudentByEmailAsync(string email)
+        {
+            return await _studentConnection.GetStudentByEmailAsync(email);
+        }
+
+        /// <summary>
+        /// Gets all students from the database by speciality
+        /// </summary>
+        /// <param name="speciality"></param>
+        /// <returns> List<Student> </returns>
+        public async Task<List<Student>?> GetStudentsbySpecialityAsync(string speciality)
+        {
+            return await _studentConnection.GetStudentsBySpecialityAsync(speciality);
+        }
+
+        /// <summary>
+        /// Gets all students from the database by class
+        /// </summary>
+        /// <param name="class"></param>
+        /// <returns> List<Student> </returns>
+        public async Task<List<Student>?> GetStudentsByClassAsync(int @class)
+        {
+            return await _studentConnection.GetStudentsByClassAsync(@class);
+        }
+
+        #endregion
+
+        #region Update
+
+        /// <summary>
+        /// Updates a student in the database
+        /// </summary>
+        /// <param name="student"></param>
+        /// <returns> bool </returns>
+        public async Task<bool> UpdateStudentAsync(Student student)
+        {
+            return await _studentConnection.UpdateStudentAsync(student);
+        }
+
+        #endregion
+
+        #region Delete
+
+        /// <summary>
+        /// Deletes a student from the database
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns> bool </returns>
+        public async Task<bool> DeleteStudentAsync(int id)
+        {
+            return await _studentConnection.DeleteStudentAsync(id);
+        }
+
+        #endregion
+
+
     }
 }
--- BlaaBog/SBO.BlaaBog.Services/Services/TeacherService.cs
+++ BlaaBog/SBO.BlaaBog.Services/Services/TeacherService.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using SBO.BlaaBog.Domain.Connections;
+using SBO.BlaaBog.Domain.Entities;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,5 +10,85 @@ namespace SBO.BlaaBog.Services.Services
 {
     public class TeacherService
     {
+        private TeacherConnection _teacherConnection;
+        public TeacherService(TeacherConnection teacherConnection)
+        {
+            _teacherConnection = new TeacherConnection();
+        }
+
+        #region Create
+
+        /// <summary>
+        /// Creates a teacher in the database
+        /// </summary>
+        /// <param name="teacher"></param>
+        /// <returns> bool </returns>
+        public async Task<bool> CreateTeacherAsync(Teacher teacher)
+        {
+            return await _teacherConnection.CreateTeacherAsync(teacher);
+        }
+
+        #endregion
+
+        #region Read
+
+        /// <summary>
+        /// Gets a teacher from the database
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns> Teacher </returns>
+        public async Task<Teacher?> GetTeacherAsync(int id)
+        {
+            return await _teacherConnection.GetTeacherAsync(id);
+        }
+
+        /// <summary>
+        /// Gets all teachers from the database
+        /// </summary>
+        /// <returns> List<Teacher> </returns>
+        public async Task<List<Teacher>?> GetTeachersAsync()
+        {
+            return await _teacherConnection.GetTeachersAsync();
+        }
+
+        /// <summary>
+        /// Gets all teachers from the database that contain the name given
+        /// </summary>
+        /// <param name="name"></param>
+        /// <returns> List<Teacher> </returns>
+        public async Task<List<Teacher>?> GetTeachersByNameAsync(string name)
+        {
+            return await _teacherConnection.GetTeachersByNameAsync(name);
+        }
+
+        #endregion
+
+        #region Update
+
+        /// <summary>
+        /// Updates a teacher in the database
+        /// </summary>
+        /// <param name="teacher"></param>
+        /// <returns> bool </returns>
+        public async Task<bool> UpdateTeacherAsync(Teacher teacher)
+        {
+            return await _teacherConnection.UpdateTeacherAsync(teacher);
+        }
+
+        #endregion
+
+        #region Delete
+
+        /// <summary>
+        /// Deletes a teacher from the database
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns> bool </returns>
+        public async Task<bool> DeleteTeacherAsync(int id)
+        {
+            return await _teacherConnection.DeleteTeacherAsync(id);
+        }
+
+        #endregion
     }
 }
