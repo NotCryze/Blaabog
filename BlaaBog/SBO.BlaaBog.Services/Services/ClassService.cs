1ecf75160c94bf5b72617e80029c86b05d30647a
Benjamin Skorstengaard <benj7698@elevcampus.dk>
2023-08-10 10:31:59 +0200

    Added service methods

--- BlaaBog/SBO.BlaaBog.Services/Services/ClassService.cs
+++ BlaaBog/SBO.BlaaBog.Services/Services/ClassService.cs
@@ -1,5 +1,8 @@
-﻿using System;
+﻿using SBO.BlaaBog.Domain.Connections;
+using SBO.BlaaBog.Domain.Entities;
+using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,5 +11,76 @@ namespace SBO.BlaaBog.Services.Services
 {
     public class ClassService
     {
+        private ClassConnection _classConnection;
+
+        public ClassService()
+        {
+            _classConnection = new ClassConnection();
+        }
+
+        #region Create Class
+
+        /// <summary>
+        /// Create a class in the database
+        /// </summary>
+        /// <param name="class"></param>
+        /// <returns>bool</returns>
+        public async Task<bool> CreateClassAsync(Class @class)
+        {
+            return await _classConnection.CreateClassAsync(@class);
+        }
+
+        #endregion
+
+        #region Read Class
+
+        /// <summary>
+        /// Get a specific class from the database
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns>Class?</returns>
+        public async Task<Class?> GetClassAsync(int id)
+        {
+            return await _classConnection.GetClassAsync(id);
+        }
+
+        /// <summary>
+        /// Get all classes from the database
+        /// </summary>
+        /// <returns>List<Class>?</returns>
+        public async Task<List<Class>?> GetClassesAsync()
+        {
+            return await _classConnection.GetClassesAsync();
+        }
+
+        #endregion
+
+        #region Update Class
+
+        /// <summary>
+        /// Update a class in the database
+        /// </summary>
+        /// <param name="class"></param>
+        /// <returns>bool</returns>
+        public async Task<bool> UpdateClassAsync(Class @class)
+        {
+            return await _classConnection.UpdateClassAsync(@class);
+        }
+
+        #endregion
+
+        #region Delete Class
+
+        /// <summary>
+        /// Delete a class from the database
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns>bool</returns>
+        public async Task<bool> DeleteClassAsync(int id)
+        {
+            return await _classConnection.DeleteClassAsync(id);
+        }
+
+        #endregion
     }
 }
--- BlaaBog/SBO.BlaaBog.Services/Services/PendingChangeService.cs
+++ BlaaBog/SBO.BlaaBog.Services/Services/PendingChangeService.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using SBO.BlaaBog.Domain.Connections;
+using SBO.BlaaBog.Domain.Entities;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,5 +10,64 @@ namespace SBO.BlaaBog.Services.Services
 {
     public class PendingChangeService
     {
+        private PendingChangeConnection _pendingChangeConnection;
+
+        public PendingChangeService()
+        {
+            _pendingChangeConnection = new PendingChangeConnection();
+        }
+
+        #region Create Pending Change
+
+        /// <summary>
+        /// Create a pending change in the database
+        /// </summary>
+        /// <param name="pendingChange"></param>
+        /// <returns>bool</returns>
+        public async Task<bool> CreatePendingChangeAsync(PendingChange pendingChange)
+        {
+            return await _pendingChangeConnection.CreatePendingChangeAsync(pendingChange);
+        }
+
+        #endregion
+
+        #region Read Pending Change
+
+        /// <summary>
+        /// Get a specific pending change from the database
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns>PendingChange?</returns>
+        public async Task<PendingChange?> GetPendingChangeAsync(int id)
+        {
+            return await _pendingChangeConnection.GetPendingChangeAsync(id);
+        }
+
+        /// <summary>
+        /// Get all pending changes from the database
+        /// </summary>
+        /// <returns>List<PendingChange>?</returns>
+        public async Task<List<PendingChange>?> GetPendingChangesAsync()
+        {
+            return await _pendingChangeConnection.GetPendingChangesAsync();
+        }
+
+        #region Update Pending Change
+
+        #endregion
+
+        #region Delete Pending Change
+
+        /// <summary>
+        /// Delete a pending change from the database
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns>bool</returns>
+        public async Task<bool> DeletePendingChangeAsync(int id)
+        {
+            return await _pendingChangeConnection.DeletePendingChangeAsync(id);
+        }
+
+        #endregion
     }
 }
