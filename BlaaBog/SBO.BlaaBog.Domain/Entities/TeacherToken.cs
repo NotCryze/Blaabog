22b708b2470214ed59da93b2e4d12371093be76e
Cryze <sammyschroeder123@gmail.com>
2023-08-14 11:14:57 +0200

    TeacherToken Entity

---
+++ BlaaBog/SBO.BlaaBog.Domain/Entities/TeacherToken.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SBO.BlaaBog.Domain.Entities
+{
+    internal class TeacherToken
+    {
+        private int? _id;
+        public int? Id { get { return _id; } }
+
+        private string _token;
+        public string Token { get { return _token; } }
+
+        private int? _teacherId;
+        public int? TeacherId { get { return _teacherId; } }
+
+        private DateTime _created_at;
+        public DateTime Created_at { get { return _created_at; } }
+
+    }
+}
