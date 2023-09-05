7c0ed3ddf6b205b2188d4658ba7f74ae4e95f9a5
benj7698 <benj7698@elevcampus.dk>
2023-09-05 10:24:01 +0200

    Added ability to create toast notifications

---
+++ BlaaBog/SBO.BlaaBog.Domain/Entities/ToastColor.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.OpenApi.Attributes;
+
+namespace SBO.BlaaBog.Domain.Entities
+{
+    public enum ToastColor
+    {
+        [Display("text-bg-primary")]
+        Primary,
+
+        [Display("text-bg-secondary")]
+        Secondary,
+
+        [Display("text-bg-success")]
+        Success,
+
+        [Display("text-bg-danger")]
+        Danger,
+
+        [Display("text-bg-warning")]
+        Warning,
+
+        [Display("text-bg-info")]
+        Info,
+
+        [Display("text-bg-light")]
+        Light,
+
+        [Display("text-bg-dark")]
+        Dark
+    }
+}
---
+++ BlaaBog/SBO.BlaaBog.Domain/Entities/ToastNotification.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SBO.BlaaBog.Domain.Entities
+{
+    public class ToastNotification
+    {
+        private ToastColor _toastStatus;
+        public ToastColor ToastStatus { get { return _toastStatus; } set { _toastStatus = value; } }
+
+        private string _message;
+        public string Message { get { return _message; } set { _message = value; } }
+    }
+}
