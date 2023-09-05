4b83144c68ef7bebff26490a86b2d2cd8512bec6
benj7698 <benj7698@elevcampus.dk>
2023-09-05 10:55:43 +0200

    Improved naming

--- BlaaBog/SBO.BlaaBog.Domain/Entities/ToastNotification.cs
+++ BlaaBog/SBO.BlaaBog.Domain/Entities/ToastNotification.cs
@@ -8,8 +8,8 @@ namespace SBO.BlaaBog.Domain.Entities
 {
     public class ToastNotification
     {
-        private ToastColor _toastStatus;
-        public ToastColor ToastStatus { get { return _toastStatus; } set { _toastStatus = value; } }
+        private ToastColor _status;
+        public ToastColor Status { get { return _toastStatus; } set { _toastStatus = value; } }
 
         private string _message;
         public string Message { get { return _message; } set { _message = value; } }
